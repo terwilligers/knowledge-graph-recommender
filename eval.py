cabe312ded1d0805c6af094bf7b3b11faeb48543
terwilligers <sfterwilliger@gmail.com>
2019-12-10 13:17:32 -0600

    added reasoning and source for ndcg at k

--- eval.py
+++ eval.py
@@ -11,11 +11,15 @@ def hit_at_k(ranked_tuples, k):
 
 def ndcg_at_k(ranked_tuples, k):
     '''
-    If the pos interaction occured in the top k scores,
-    return the ndcg relative rank value
-    Taken from papers implementation
+    Article on ndcg: http://ethen8181.github.io/machine-learning/recsys/2_implicit.html
+    ndcg_k = DCG_k / IDCG_k
+    Say i represents index of or tag=1 in the top k, then since only one contribution to summation
+    DCG_k = rel_i / log(i+1)
+    IDCG_k = rel_i / log(1+1) since the first item is best rank
+    ndcg_k = log(2) / log(i+1)
+    Note we use log(2) / log(i+2) since indexing from 0
     '''
-    for i,(score, tag) in enumerate(ranked_tuples):
+    for i,(score, tag) in enumerate(ranked_tuples[:k]):
         if tag == 1:
             return math.log(2) / math.log(i + 2)
     return 0
