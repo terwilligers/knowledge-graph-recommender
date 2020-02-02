83b419728045663a57a2b4032f983d5163845375
JoeyCG <joey.ck6@gmail.com>
2020-02-02 11:17:44 -0600

    minor bug fixes in data conversion. Turns out randint is inclusive on both ends...

--- baseline/data_conversion.py
+++ baseline/data_conversion.py
@@ -15,26 +15,26 @@ with open("../data/song_data_ix/rs_ix_song_user.dict", 'rb')as handle:
     full_song_user = pickle.load(handle)
 
 # converts pos/neg usersong pair lists into a matrix where every row contains 101 tuples with the format
-# ((song, user) 1 or 0)
+# ((user, song), 1 or 0)
 # each row has 1 positive interactions and 100 negative interactions
 
 
 def convert_for_bpr(pos_list, neg_list):
     bpr_matrix = []
     percent = 0
-    while len(neg_list) > 99:
+    while (len(neg_list) > 99) and (len(pos_list) != 0) and (percent < 10):
         if len(neg_list) % 15573 == 0:
             print(percent, "%")
             percent += 1
         row = []
         for i in range(100):
             rand_neg_user = neg_list.pop(randint(0, len(neg_list) - 1))
-            row.append(((rand_neg_user[1], rand_neg_user[0]), 0))
+            row.append((rand_neg_user, 0))
         rand_pos_user = pos_list.pop(randint(0, len(pos_list) - 1))
-        row.insert(randint(0, 100), ((rand_pos_user[1], rand_pos_user[0]), 1))
+        row.insert(randint(0, 99), (rand_pos_user, 1))
         bpr_matrix.append(row)
     mat = np.array(bpr_matrix)
-    np.save('bpr_matrix', mat)
+    np.save('10_percent_bpr_matrix', mat)
 
 
 def main():
@@ -46,4 +46,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
