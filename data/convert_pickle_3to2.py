bf372c807dfe639f74ea27aaf752349b629cd285
rosafish <zhour@carleton.edu>
2020-02-06 00:39:46 -0600

    filter out users who do not listen to any song, count songs not listend by any user as negative in eval, allow eval on training data

--- data/convert_pickle_3to2.py
+++ data/convert_pickle_3to2.py
@@ -1,5 +1,5 @@
 import pickle
-'''
+
 with open("song_data_ix/dense_train_ix_user_song.dict", 'rb') as handle:
     train_user_song = pickle.load(handle)
 with open("song_data_ix/dense_test_ix_user_song.dict", 'rb') as handle:
@@ -10,7 +10,7 @@ with open("song_data_ix/dense_ix_song_user.dict", 'rb') as handle:
 pickle.dump(train_user_song, open("song_data_ix/dense_train_ix_user_song_py2.pkl","wb"), protocol=2)
 pickle.dump(test_user_song, open("song_data_ix/dense_test_ix_user_song_py2.pkl","wb"), protocol=2)
 pickle.dump(full_song_user, open("song_data_ix/dense_ix_song_user_py2.pkl","wb"), protocol=2)
-'''
+
 with open("song_data_ix/rs_train_ix_user_song.dict", 'rb') as handle:
     train_user_song = pickle.load(handle)
 with open("song_data_ix/rs_test_ix_user_song.dict", 'rb') as handle:
